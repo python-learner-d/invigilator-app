4657780dbf3f121acdb49245072ba16ed764194b
subbu <msm9652@gmail.com>
2025-07-24 22:43:39 +0530

    Add automatic superuser creation for deployment

---
+++ attendance/management/commands/createsuperuser_from_env.py
@@ -0,0 +1,25 @@
+# attendance/management/commands/createsuperuser_from_env.py
+import os
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Command(BaseCommand):
+    help = 'Creates a superuser from environment variables.'
+
+    def handle(self, *args, **options):
+        username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+        email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+        if not all([username, email, password]):
+            self.stdout.write(self.style.ERROR('Missing superuser environment variables.'))
+            return
+
+        if User.objects.filter(username=username).exists():
+            self.stdout.write(self.style.SUCCESS(f'Superuser "{username}" already exists.'))
+        else:
+            User.objects.create_superuser(username=username, email=email, password=password)
+            self.stdout.write(self.style.SUCCESS(f'Successfully created superuser "{username}".'))
+
